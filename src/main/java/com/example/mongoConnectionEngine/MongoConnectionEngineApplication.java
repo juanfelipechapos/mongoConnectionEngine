6fedf256a4409d74f17e51a6c6ce4aac0936a660
felipe <juanfelipe2217@gmail.com>
2021-03-14 23:39:35 -0500

    Initial commit

---
+++ src/main/java/com/example/mongoConnectionEngine/MongoConnectionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoConnectionEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoConnectionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoConnectionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mongoConnectionEngine/MongoConnectionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongoConnectionEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoConnectionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
